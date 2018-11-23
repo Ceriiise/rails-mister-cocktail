e25eb68e6ba6567c1ade0a53b8105b6d20685908
Philippine Berton <Phi.berton@hotmail.fr>
2018-11-23 12:35:36 +0100

    mise a jour exo

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,22 +1,30 @@
 class DosesController < ApplicationController
-  def new
-    @cocktail = Cocktail.find(params[:id])
-    @dose = Dose.new
-  end
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new
+  # end
 
   def create
-    @cocktail = Cocktail.find(params[:id])
-    @dose.ingredient = cocktail_params[:ingredient]
-    @dose.cocktail = cocktail_params[:cocktail]
-    raise
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @review = Review.new
+      render "cocktails/show"
+    end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
-  def cocktail_params
-    params.require(:cocktail).permit(:name)
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,55 +1,64 @@
 require 'json'
 require 'open-uri'
 
-puts 'Cleaning database...'
+puts 'DB Destroy ----------'
+Dose.destroy_all
 Cocktail.destroy_all
 Ingredient.destroy_all
-Dose.destroy_all
+puts 'Done!'
 
-#SEED COCKTAILS (NAME & PHOTO)
-puts 'Creating COCKTAILS (NAME & PHOTO)...'
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
-file = open(url).read
-cocktail_list = JSON.parse(file)
+puts 'Creating Ingredients...'
 
-array_cocktails = cocktail_list.values.flatten
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+user_serialized = open(url).read
+result = JSON.parse(user_serialized)
+  result['drinks'].each do |item|
+    Ingredient.create!(name: item["strIngredient1"])
+    # p item['strIngredient1']
+  end
 
-array_cocktails.each do |cocktail|
-  Cocktail.create(name: cocktail.values.first, photo: cocktail.values.second)
+puts 'Done!!'
+puts 'Creating Cocktails & Doses...'
 
-  # FIND COCKTAILS BY ID
-  url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{cocktail.values.third}"
-  puts url
-  # url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=13060"
-  file = open(url).read
-  cocktail_details = JSON.parse(file)
+  cocktails_url = Array.new
+  url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
+  user_serialized = open(url).read
+  result = JSON.parse(user_serialized)
+    result['drinks'].each do |item|
+      cocktail_url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{item["idDrink"]}"
+      cocktails_url << cocktail_url
+    end
 
-  array_details = cocktail_details.values.flatten
+  cocktails_url.each do |url|
+    cocktail_ingredients = []
+    user_serialized = open(url).read
+    result = JSON.parse(user_serialized)
+      cocktail = Cocktail.new(name: result['drinks'][0]['strDrink'])
+      cocktail.description = result['drinks'][0]['strInstructions']
+      cocktail.image_url = result['drinks'][0]["strDrinkThumb"]
+      result['drinks'][0].each do |item|
+        if (/strIngredient/).match?(item[0])
+          cocktail_ingredients << item[1]
+        end
+      end
 
-  array_details.each do |detail|
-    # p detail.values.last(31).first
-    # p detail.values.last(31).second
-    # p detail.values.last(31).third
-    # p detail.values.last(31).fourth
-
-    # cocktail_name = detail.values.second
-
-    # SEED INGREDIENTS
-    cocktail_ingredients = detail.values.last(31).first(15)
-    p cocktail_ingredients
-
-    cocktail_ingredients.each do |ingredient|
-      Ingredient.create(name: ingredient)
-
-      # SEED DOSES
-      cocktail_doses = detail.values.last(16).first(15)
-      cocktail_doses.each do |dose|
-        Dose.create(description: dose, ingredient_id: Ingredient.where(name: ingredient))
-        # d = Dose.new(description: dose)
-        # d.ingredient = Ingredient.where(name: ingredient)
-        # d.save
+    cocktail_ingredients.reject!(&:blank?).each do |ingredient|
+      if Ingredient.where(name: ingredient).count == 1
+        i = cocktail_ingredients.find_index(ingredient)
+        dose = Dose.new(description: result['drinks'][0]["strMeasure#{i+1}"])
+        dose.ingredient_id = Ingredient.where(name: ingredient).first.id
+        dose.cocktail = cocktail
+        dose.save
+      else
+        new_ingredient = Ingredient.create!(name: ingredient)
+        i = cocktail_ingredients.find_index(ingredient)
+        dose = Dose.new(description: result['drinks'][0]["strMeasure#{i+1}"])
+        dose.ingredient_id = Ingredient.where(name: ingredient).first.id
+        dose.cocktail = cocktail
+        dose.save
       end
     end
   end
-end
+
+puts 'Done!!!'
